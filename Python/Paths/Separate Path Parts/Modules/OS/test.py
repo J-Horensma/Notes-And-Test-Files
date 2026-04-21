c0cb9beca933a680da61f902ff7a72fe674c472e
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2026-04-21 11:58:27 -0700

    Add files via upload

---
+++ Python/Paths/Separate Path Parts/Modules/OS/test.py
@@ -0,0 +1,63 @@
+from os.path import exists, isfile, isdir, basename, splitdrive, splitext
+from time import sleep
+from platform import system
+
+#IF THIS FILE, IS LAUNCHED BY WINDOWS:
+if system() == 'Windows':
+    from colorama import just_fix_windows_console
+    just_fix_windows_console()
+
+def clear_console():
+   print('\033[H\033[2J', end='', flush=True)
+
+def close():
+    clear_console()
+    print('Exiting...')
+    sleep(1)
+    exit(0)
+
+try:
+    while True:
+        clear_console()
+        PATH = input('Enter a path, to separate: ')
+        if not PATH.strip():
+            print('Empty input, was provided\n')
+            input('Press Enter, to try again: ')
+            clear_console()
+            continue
+        elif not exists(PATH):
+            print(f'The following path, does not exist:\n{PATH}\n')
+            input('Press Enter, to try again: ')
+            clear_console()
+            continue
+        PATH_TYPE = 'File' if isfile(PATH) else 'Directory' if isdir(PATH) else 'Unknown'
+        DRIVE, MIDDLE_AND_END = splitdrive(PATH)
+        print('Path type:')
+        print('----------')
+        print(f'{PATH_TYPE}\n')
+        print('Path drive:')
+        print('-----------')
+        print(f'{DRIVE}\n')
+        if isfile(PATH):
+            print('File name:')
+            print('----------')
+            print(basename(PATH))
+            print(f'File path:\n{PATH}\n')
+            print('File extension:')
+            print('---------------')
+            print(f'{splitext(PATH)}\n')
+        elif isdir(PATH):
+            print('Directory name:')
+            print('---------------')
+            print(f'{basename(PATH)}\n')
+            print('Directory path:')
+            print('---------------')
+            print(f'{PATH}\n')
+        input('Press Enter, to try again: ')
+except KeyboardInterrupt:
+    close()
+except Exception as ERROR:
+    clear_console()
+    print(f'{ERROR}\n')
+    input('Press Enter, to exit: ')
+    exit(1)
