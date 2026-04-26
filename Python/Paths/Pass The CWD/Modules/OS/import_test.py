7e76ff6b79f71addd3d23724fb628b9f1d3dd9be
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2026-04-25 19:08:53 -0700

    Delete Python directory

--- Python/Paths/Pass The CWD/Modules/OS/import_test.py
+++
@@ -1,9 +0,0 @@
-from os import chdir
-from os.path import dirname
-
-#WHEN LAUNCHING A PYTHON FILE, IMPORTING ANOTHER PYTHON FILE,
-#THAT DETECTS THE CWD OF THE IMPORTING PYTHON FILE, WITH "sys.argv[0]",
-#MAKE SURE "os.chdir(os.path.dirname(__file__))", IS USED BEFORE IMPORTING,
-#SO "sys.argv[0]" DOESN'T DETECT THE DEFAULT PYTHON PROFILE PATH.
-chdir(dirname(__file__))
-import direct_test
